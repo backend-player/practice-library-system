2de10d6964a14767d473ae19a4209540bd512e3c
backend-player <rizkylev@gmail.com>
2022-08-15 21:58:35 +0700

    add borrower data filter function

--- data-peminjam.php
+++ data-peminjam.php
@@ -9,6 +9,19 @@ if( !isset($_SESSION["username"]) ) {
 $sql = "SELECT * FROM data_peminjam WHERE status_peminjaman = 'aktif' ";
 $result = mysqli_query($conn, $sql);
 
+$filter_status_peminjaman = "aktif";
+
+if(isset($_GET["submit_filter"])) {
+    $filter_status_peminjaman = htmlspecialchars( $_GET["filter_status_peminjaman"] );
+    if($filter_status_peminjaman == "aktif") {
+        $sql = "SELECT * FROM data_peminjam WHERE status_peminjaman = 'aktif' ";
+        $result = mysqli_query($conn, $sql);
+    } else if($filter_status_peminjaman == "selesai") {
+        $sql = "SELECT * FROM data_peminjam WHERE status_peminjaman = 'selesai' ";
+        $result = mysqli_query($conn, $sql);
+    }
+}
+
 if(isset($_GET["submit_cari"])) {
     if(empty    ($_GET["cari"])) {
         $error_cari =  "anda belum mengisi pencarian";
@@ -40,6 +53,17 @@ if(isset($_GET["submit_cari"])) {
     <br>
 
     <form action="" method="GET">
+        <label for="filter_status_peminjaman">Filter berdasarkan status peminjaman:</label>
+        <select name="filter_status_peminjaman">
+            <option value="aktif" <?php if(isset($_GET["filter_status_peminjaman"]) && $_GET["filter_status_peminjaman"] == "aktif") echo "selected = 'selected' "; ?> >Aktif</option>
+            <option value="selesai" <?php if(isset($_GET["filter_status_peminjaman"]) && $_GET["filter_status_peminjaman"] == "selesai") echo "selected = 'selected' "; ?> >Selesai</option>
+        </select>
+        <input type="submit" value="filter" name="submit_filter">
+    </form>
+
+    <br>
+
+    <form action="" method="GET">
         <input type="text" name="cari" id="" placeholder="Cari data peminjam" style="width: 240px;">
         <input type="submit" value="cari" name="submit_cari">
     </form>
@@ -51,6 +75,7 @@ if(isset($_GET["submit_cari"])) {
 
     <br><br><br>
 
+    <?php if($filter_status_peminjaman == "aktif"): ?>
     <table>
         <tr>
             <th>Nama</th>
@@ -71,6 +96,7 @@ if(isset($_GET["submit_cari"])) {
             $row["waktu_pengembalian"] = "-";
             }
             ?>
+
             <tr>
                 <td><?php echo $row["nama"] ?></td>
                 <td><?php echo $row["judul_buku"] ?></td>
@@ -79,11 +105,50 @@ if(isset($_GET["submit_cari"])) {
                 <td><?php echo "Rp. " . $row["jumlah_denda"] ?></td>
                 <td><?php echo $row["status_peminjaman"] ?></td>
                 <td><?php echo $row["jumlah_buku_dipinjam"] ?></td>
-                <td><a href="kembalikan-buku.php?id=<?php echo $row["id"] ?>" onclick="return confirm('Anda yakin ingin mengupdate data?')">selesai</a></td>
+                <td><a class="tombol-selesai" href="kembalikan-buku.php?id=<?php echo $row["id"] ?>" onclick="return confirm('Anda yakin ingin mengupdate data?')">selesai</a></td>
             </tr>
+            
         <?php endwhile ?>
 
     </table>
+    <?php endif ?>
+
+
+    <?php if($filter_status_peminjaman == "selesai"): ?>
+    <table>
+        <tr>
+            <th>Nama</th>
+            <th>Judul buku</th>
+            <th>Waktu peminjaman</th>
+            <th>Waktu pengembalian</th>
+            <th>Jumlah denda</th>
+            <th>Status peminjaman</th>
+            <th>Jumlah buku dipinjam</th>
+        </tr>
+
+        <?php while($row = mysqli_fetch_assoc($result)): ?>
+
+            <?php
+            // jika waktu pengembalian masih kosong, ubah data yang ditampilkan menjadi -
+            if($row["waktu_pengembalian"] == "0000-00-00 00:00:00") {
+            $row["waktu_pengembalian"] = "-";
+            }
+            ?>
+
+            <tr>
+                <td><?php echo $row["nama"] ?></td>
+                <td><?php echo $row["judul_buku"] ?></td>
+                <td><?php echo $row["waktu_peminjaman"] ?></td>
+                <td><?php echo $row["waktu_pengembalian"] ?></td>
+                <td><?php echo "Rp. " . $row["jumlah_denda"] ?></td>
+                <td><?php echo $row["status_peminjaman"] ?></td>
+                <td><?php echo $row["jumlah_buku_dipinjam"] ?></td>
+            </tr>
+            
+        <?php endwhile ?>
+
+    </table>
+    <?php endif ?>
 
 </body>
 </html>
--- kembalikan-buku.php
+++ kembalikan-buku.php
@@ -9,19 +9,29 @@ if( !isset($_SESSION["username"]) ) {
 $id = $_GET["id"];
 
 $timezone = time() + (60 * 60 * 7);
-$waktu_pengembalian = gmdate("Y/m/d H:i:sa", $timezone);
+$waktu_pengembalian = gmdate("Y/m/d H:i:s", $timezone);
 
 // ambil judul buku yang dipinjam
-$sql = "SELECT judul_buku FROM data_peminjam WHERE id = '$id' ";
+$sql = "SELECT judul_buku, waktu_peminjaman FROM data_peminjam WHERE id = '$id' ";
 $result = mysqli_query($conn, $sql);
 
 while($row = mysqli_fetch_assoc($result)) {
     $judul = $row["judul_buku"];
+    $waktu_peminjaman = $row["waktu_peminjaman"];
 }
 
+// hitung waktu keterlambatan pengembalian buku
+$waktu_awal = strtotime($waktu_peminjaman);
+$waktu_akhir = strtotime($waktu_pengembalian);
+
+$perbedaan_hari = $waktu_akhir - $waktu_awal;
+$jumlah_hari = $perbedaan_hari / 86400;
+$jumlah_hari = round($jumlah_hari);
+
+$denda = $jumlah_hari * 2000;
 
 // ubah status peminjaman menjadi selesai, masukkan waktu pengembalian, kurangi 1 jumlah peminjaman buku
-$sql = "UPDATE data_peminjam SET status_peminjaman = 'selesai', waktu_pengembalian = '$waktu_pengembalian', jumlah_buku_dipinjam = jumlah_buku_dipinjam - 1 WHERE id = '$id' ";
+$sql = "UPDATE data_peminjam SET status_peminjaman = 'selesai', waktu_pengembalian = '$waktu_pengembalian',jumlah_denda = '$denda' , jumlah_buku_dipinjam = jumlah_buku_dipinjam - 1 WHERE id = '$id' ";
 $result = mysqli_query($conn, $sql);
 
 // tambahkan jumlah buku yang dikembalikan ke jumlah buku yang tersedia
@@ -33,6 +43,13 @@ if($result) {
               alert('Data berhasil diupdate'); 
               window.location.href='data-peminjam.php';
             </script>";
+    // echo "waktu awal: " . $waktu_awal;
+    // echo "<br>";
+    // echo "waktu akhir: " . $waktu_akhir;
+    // echo "<br>";
+    // echo "waktu pengembalian: " . $waktu_pengembalian;
+    // echo "<br>";
+    // echo "waktu peminjaman: " . $waktu_peminjaman;
   } else {
     echo "<script>
             alert('Data gagal diupdate'); 
